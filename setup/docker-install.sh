ee295e99f9acfabd05849ed0b17a51c02b437e01
Adriano Vieira <adriano.svieira@gmail.com>
2016-06-29 18:24:13 -0300

    Update docker-install.sh to check os-verision support

--- setup/docker-install.sh
+++ setup/docker-install.sh
@@ -1,5 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
+if [[ -f /etc/os-release  ]]; then
+  . /etc/os-release
+fi
+
+if [[ "$ID" == "centos" && "$VERSION_ID" == "7" ]]; then
 echo "INFO: [docker-install.sh] importing gpg key from dockerproject"
 sudo rpm --import https://yum.dockerproject.org/gpg
 sudo yum clean all
@@ -11,3 +16,7 @@ sudo yum install -y -q docker-engine
 
 echo "INFO: [docker-install.sh] start docker-engine"
 sudo systemctl enable docker && sudo systemctl start docker
+else
+  echo "ERROR: [docker-install.sh] OS [$ID-$VERSION_ID] not supported!"
+  exit 1
+fi
