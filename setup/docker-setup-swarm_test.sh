e5f217224a81ce0550bbe563a560687fc1aecece
Adriano Vieira <adriano.svieira@gmail.com>
2016-07-13 16:41:12 -0300

    Add "hello world" wsgi flask app

---
+++ setup/docker-setup-swarm_test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -x
+
+echo "INFO: [docker-setup-swarm_test.sh] create app flask+kafka service"
+docker service create -p 80:80 --replicas 3 --name flask_kafka adrianovieira/flask_kafka:debian8
+#docker service create -p 80:80 --replicas 1 --name flask_kafka adrianovieira/flask_kafka:python27
+#docker service create -p 80:80 --replicas 1 --name flask_kafka adrianovieira/flask_kafka:ubuntu16
+
+echo "INFO: [docker-setup-swarm_test.sh] finished successfuly"
