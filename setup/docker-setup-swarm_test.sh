5d4638840e2a12b6f94946df9a92a279a12d18c3
Adriano Vieira <adriano.svieira@gmail.com>
2016-07-15 19:08:30 -0300

    Update swarm_test script to pull test images

--- setup/docker-setup-swarm_test.sh
+++ setup/docker-setup-swarm_test.sh
@@ -1,4 +1,8 @@
-#!/bin/bash -x
+#!/bin/bash
+
+echo "INFO: [docker-setup-swarm_test.sh] pull images"
+docker pull adrianovieira/flask_kafka
+docker pull adrianovieira/flask_kafka:0.1
 
 echo "INFO: [docker-setup-swarm_test.sh] create app flask+kafka service"
 docker service create -p 80:80 --replicas 3 --name flask_kafka adrianovieira/flask_kafka
