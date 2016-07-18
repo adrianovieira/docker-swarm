d5e152f0eadabe7d99e5cdf4305951f9cf9cc162
Adriano Vieira <adriano.svieira@gmail.com>
2016-07-17 22:25:08 -0300

    Set a simple secret value needed to accept nodes into cluster

--- setup/docker-setup-swarm_manager.sh
+++ setup/docker-setup-swarm_manager.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo "INFO: [docker-setup-swarm_manager.sh] setting up swarm manager"
-docker swarm init --listen-addr $1:$2
+docker swarm init --secret 1qasw23edfr45tghy6 --listen-addr $1:$2
--- setup/docker-setup-swarm_worker.sh
+++ setup/docker-setup-swarm_worker.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo "INFO: [docker-setup-swarm_worker.sh] setting up swarm worker"
-docker swarm join $1:$2
+docker swarm join --secret 1qasw23edfr45tghy6 $1:$2
